0ad3b670afcc9e2051e91929c71dac948daa4095
yuka7110 <yuka.o.nep@gmail.com>
2021-01-23 11:36:10 +0900

    Revert "各モデルのテスト"
    This reverts commit 37c83f1a17e6bfc1bbf6b6ddfdf18c8e2a4ec922.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,11 @@ class UsersController < ApplicationController
       redirect_to root_path
     else
       render :edit
+
     end
   end
+
+  
 
   private
 
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,9 +3,9 @@ class Message < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :content, presence: true, unless: :was_attached?
+  validates :content, presence: true, unless: was_attached?
 
   def was_attached?
-    self.image.attached?
+    self.image.was_attached?
   end
 end
